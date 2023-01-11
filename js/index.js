9feeb02007121b20e5b192fdaa017db26df477c1
radin-allahverdian <radin.allahverdian83@gmail.com>
2023-01-11 20:25:23 +0330

    navigation got completed

--- js/index.js
+++ js/index.js
@@ -13,3 +13,18 @@ itemClicker[2].addEventListener("click", function () {
 itemClicker[3].addEventListener("click", function () {
     itemUL[3].classList.toggle("hidden-height");
 });
+
+const MenuIcon = document.querySelector(".MenuIcon");
+const NavigationHeight = document.querySelector(".navigation-height");
+let trueORfalse = true;
+MenuIcon.addEventListener("click", function () {
+    if (trueORfalse == true) {
+        NavigationHeight.classList.remove("hidden-navigation");
+        trueORfalse = false;
+        document.body.style.overflow = "hidden";
+    } else if (trueORfalse == false) {
+        NavigationHeight.classList.add("hidden-navigation");
+        trueORfalse = true;
+        document.body.style.overflow = "auto";
+    }
+});
