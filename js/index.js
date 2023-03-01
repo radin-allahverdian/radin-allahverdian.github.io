c043681e05342618cd895c4c6184a59e32e6c23e
radin-allahverdian <radin.allahverdian83@gmail.com>
2023-03-01 20:43:10 +0330

    a

--- js/index.js
+++ js/index.js
@@ -7,28 +7,24 @@ headerPart[0].addEventListener("click", function () {
     myUl[1].classList.add("hidden-ul");
     myUl[2].classList.add("hidden-ul");
     myUl[3].classList.add("hidden-ul");
-    PlusIcon[0].classList.toggle("plusiconstyle");
 });
 headerPart[1].addEventListener("click", function () {
     myUl[1].classList.toggle("hidden-ul");
     myUl[0].classList.add("hidden-ul");
     myUl[2].classList.add("hidden-ul");
     myUl[3].classList.add("hidden-ul");
-    PlusIcon[1].classList.toggle("plusiconstyle");
 });
 headerPart[2].addEventListener("click", function () {
     myUl[2].classList.toggle("hidden-ul");
     myUl[0].classList.add("hidden-ul");
     myUl[1].classList.add("hidden-ul");
     myUl[3].classList.add("hidden-ul");
-    PlusIcon[2].classList.toggle("plusiconstyle");
 });
 headerPart[3].addEventListener("click", function () {
     myUl[3].classList.toggle("hidden-ul");
     myUl[0].classList.add("hidden-ul");
     myUl[1].classList.add("hidden-ul");
     myUl[2].classList.add("hidden-ul");
-    PlusIcon[3].classList.toggle("plusiconstyle");
 });
 
 const MenuIcon = document.querySelector(".MenuIcon");
