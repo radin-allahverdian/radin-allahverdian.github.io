b92505be1b92066efe74efba900d25b0d50f93be
radin-allahverdian <radin.allahverdian83@gmail.com>
2023-03-01 20:33:02 +0330

    footer is finished

--- js/index.js
+++ js/index.js
@@ -1,17 +1,34 @@
 const headerPart = document.querySelectorAll(".header-part");
 const myUl = document.querySelectorAll(".MyUl");
+const PlusIcon = document.querySelectorAll(".PlusIcon");
 
 headerPart[0].addEventListener("click", function () {
     myUl[0].classList.toggle("hidden-ul");
+    myUl[1].classList.add("hidden-ul");
+    myUl[2].classList.add("hidden-ul");
+    myUl[3].classList.add("hidden-ul");
+    PlusIcon[0].classList.toggle("plusiconstyle");
 });
 headerPart[1].addEventListener("click", function () {
     myUl[1].classList.toggle("hidden-ul");
+    myUl[0].classList.add("hidden-ul");
+    myUl[2].classList.add("hidden-ul");
+    myUl[3].classList.add("hidden-ul");
+    PlusIcon[1].classList.toggle("plusiconstyle");
 });
 headerPart[2].addEventListener("click", function () {
     myUl[2].classList.toggle("hidden-ul");
+    myUl[0].classList.add("hidden-ul");
+    myUl[1].classList.add("hidden-ul");
+    myUl[3].classList.add("hidden-ul");
+    PlusIcon[2].classList.toggle("plusiconstyle");
 });
 headerPart[3].addEventListener("click", function () {
     myUl[3].classList.toggle("hidden-ul");
+    myUl[0].classList.add("hidden-ul");
+    myUl[1].classList.add("hidden-ul");
+    myUl[2].classList.add("hidden-ul");
+    PlusIcon[3].classList.toggle("plusiconstyle");
 });
 
 const MenuIcon = document.querySelector(".MenuIcon");
